326f026e461f1bc2a3dc8424ce5c97496ed2cc91
savithamk <80167929+savithamk@users.noreply.github.com>
2022-03-19 06:00:23 -0400

    Added functionality for add, delete and reorder a movie in a section

--- TableView/MovieList.swift
+++ TableView/MovieList.swift
@@ -8,5 +8,29 @@
 import Foundation
 
 class MovieList {
-   
+    
+    //setting initial list of movies
+    init(autofil: Bool){
+        if autofil {
+            movies = ["Eternals","Dune","No Time To Die","Last Night in Soho","Ron's Done Wrong","Halloween Kills","Venom","Antlers","The Addams Family 2"]
+        }
+    }
+
+    //add movie to movie list
+    func addMovie(movie: Movie){
+        movies.append(movie.title)
+        
+    }
+    
+    //add movie from movie list
+    func removeMovie(indexPath: IndexPath){
+        movies.remove(at: indexPath.row)
+    }
+    
+    //reorder movies in the section
+    func moveMovie(fromIndexPath: IndexPath, toIndexPath: IndexPath){
+        let temp = movies[fromIndexPath.row]
+        movies.remove(at: fromIndexPath.row)
+        movies.insert(temp, at: toIndexPath.row)
+    }
 }
